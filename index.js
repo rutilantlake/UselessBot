2f365f350b6d3de5130c88a015bd5b5d7400ea14
RutilantLake <vincent.verwee@live.be>
2020-07-16 18:03:53 +0200

    useless bot

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ client.on('message', message => {
 
 
 client.on('message', message => {
-    if (!message.content.startsWith(PREFIX) || message.author.bot) return;
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
