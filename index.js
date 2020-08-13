db6181cd890c6a3b8e2b0dde07fdcfe48e6cbfa0
RutilantLake <vincent.verwee@live.be>
2020-08-13 11:19:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -47,6 +47,8 @@ client.on('message', message => {
         message.channel.send('https://tenor.com/view/owo-whats-this-intensifies-mad-gif-12266002')
     } else if (message.content === ":3") {
         message.channel.send('https://tenor.com/view/nekopara-anime-smile-happy-delighted-gif-16605255')
+    } else if(message.content.includes === 'rem'){
+        message.channel.send("https://res.cloudinary.com/teepublic/image/private/s--JLtdSyZA--/t_Preview/b_rgb:c8e0ec,c_limit,f_jpg,h_630,q_90,w_630/v1559153268/production/designs/4953243_0.jpg")
     }
 })
 
