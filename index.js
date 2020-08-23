37931ff663ff683bc972729a9a232bee28db9614
RutilantLake <vincent.verwee@live.be>
2020-08-23 20:08:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -260,6 +260,7 @@ client.on('message', message => {
     } else if (command === 'big') {
         if (args[0] === 'brain') {
             message.channel.send('https://tenor.com/view/anime-anime-glasses-stare-glasses-gif-15313333');
+        }
     } else if(command === 'cute'){
         message.channel.send('https://tenor.com/view/sailor-moon-suit-old-man-peace-sign-sailor-scout-anime-gif-14298094');
     } else if(command === 'hot'){
