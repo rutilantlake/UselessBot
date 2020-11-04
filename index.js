0f48d0cbf644c53e522139e0941d55e235632428
RutilantLake <vincent.verwee@live.be>
2020-11-04 12:59:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -57,8 +57,6 @@ client.on('message', message => {
         message.channel.send('https://tenor.com/view/jojo-bizarre-jojos-adventure-fbi-on-the-other-side-gif-13841785');
     } else if (message.content === 'interesting') {
         message.channel.send('https://tenor.com/view/hmmm-mmmm-interesting-batman-gif-11990772');
-    } else if (message.content.includes('you')) {
-        message.channel.send('https://cdn.discordapp.com/attachments/674614766886453278/759795050003038209/Naamloos-1.png');
     } else if (message.content === ':o') {
         message.channel.send('https://media.tenor.co/videos/23da04e9110886fe3755abc78ea042ac/mp4')
     } else if (message.content === 'hm hm') {
@@ -283,6 +281,8 @@ client.on('message', message => {
                 message.channel.send('https://tenor.com/view/jojo-do-you-understand-jojos-bizarre-adventure-anime-slime-gif-17398513')
             }
         }
+    } else if(command === 'you'){
+        message.channel.send('https://cdn.discordapp.com/attachments/674614766886453278/759795050003038209/Naamloos-1.png');
     }
 })
 
